07a0dc142d01b99a5ffa2519b8be04da4e948b51
Vladimir Panteleev <git@thecybershadow.net>
2018-05-02 00:07:24 +0000

    genship: Search in parallel

--- genship.d
+++ genship.d
@@ -2,6 +2,7 @@ import std.algorithm.iteration;
 import std.algorithm.searching;
 import std.conv;
 import std.exception;
+import std.parallelism;
 import std.random;
 import std.range;
 import std.stdio;
@@ -12,9 +13,6 @@ import shipdata;
 
 void main()
 {
-	Xorshift rng;
-	rng.seed(unpredictableSeed);
-
 	auto numOutfits = shipData.items.length - shipData.numShips;
 	auto maxIterations = numOutfits * numOutfits;
 
@@ -22,50 +20,65 @@ void main()
 	enforce(outfitsExpansions.length == 1);
 
 	Score bestScore;
+	uint outerIterations = 0;
+	auto maxOuterIterations = maxIterations;
 
-	while (true)
+	foreach (thread; totalCPUs.iota.parallel(1))
 	{
-		Config config;
-		config.add(uniform(0, shipData.numShips, rng));
-		Score score = config.score;
+		Xorshift rng;
+		rng.seed(unpredictableSeed);
 
-		uint iterations;
-		while (iterations < maxIterations)
+		while (true)
 		{
-			auto newConfig = config;
-			foreach (n; 0..uniform(0, 3, rng))
-				if (newConfig.numItems > 1)
-					newConfig.remove(uniform(1, newConfig.numItems, rng));
+			Config config;
+			config.add(uniform(0, shipData.numShips, rng));
+			Score score = config.score;
 
-			foreach (n; 0..uniform(0, 3, rng))
-				if (newConfig.numItems < maxOutfits)
-					newConfig.add(uniform(shipData.numShips, cast(ItemIndex)shipData.items.length, rng));
-
-			// Try to fix this configuration (optimization)
-			while (newConfig.numItems < maxOutfits && newConfig.stats.attributes[Attribute.outfitSpace] < 0)
-				newConfig.add(outfitsExpansions[0]);
-
-			if (newConfig.isOK)
+			uint iterations;
+			while (iterations < maxIterations)
 			{
-				auto newScore = newConfig.score;
-				if (score < newScore)
-				{
-					config = newConfig;
-					score = newScore;
-					iterations = 0;
-				}
-			}
-			iterations++;
-		}
+				auto newConfig = config;
+				foreach (n; 0..uniform(0, 3, rng))
+					if (newConfig.numItems > 1)
+						newConfig.remove(uniform(1, newConfig.numItems, rng));
 
-		if (bestScore < score)
-		{
-			bestScore = score;
-			writeln("\n\n############################################################################################################################################\n");
-			printConfig(config);
-			config.save("genship.json");
-			createSave(`/home/vladimir/Sync-PC/saves/endless-sky/saves/Test Drive.txt`, [config]);
+				foreach (n; 0..uniform(0, 3, rng))
+					if (newConfig.numItems < maxOutfits)
+						newConfig.add(uniform(shipData.numShips, cast(ItemIndex)shipData.items.length, rng));
+
+				// Try to fix this configuration (optimization)
+				while (newConfig.numItems < maxOutfits && newConfig.stats.attributes[Attribute.outfitSpace] < 0)
+					newConfig.add(outfitsExpansions[0]);
+
+				if (newConfig.isOK)
+				{
+					auto newScore = newConfig.score;
+					if (score < newScore)
+					{
+						config = newConfig;
+						score = newScore;
+						iterations = 0;
+					}
+				}
+				iterations++;
+			}
+
+			synchronized
+			{
+				if (bestScore < score)
+				{
+					bestScore = score;
+					writeln("\n\n############################################################################################################################################\n");
+					printConfig(config);
+					config.save("genship.json");
+					createSave(`/home/vladimir/Sync-PC/saves/endless-sky/saves/Test Drive.txt`, [config]);
+					outerIterations = 0;
+				}
+				else
+					if (outerIterations++ >= maxOuterIterations)
+						break;
+			}
+			//else { write("."); stdout.flush(); }
 		}
-		//else { write("."); stdout.flush(); }
 	}
 }
