95b20436fff77e99cfc3ece6d38440f82bd5e05f
Vladimir Panteleev <git@thecybershadow.net>
2018-05-03 13:52:57 +0000

    savewriter: Fix copying explosions

--- savewriter.d
+++ savewriter.d
@@ -49,7 +49,7 @@ void createSave(string fn, Config[] configs)
 			if (node.isValue)
 				f.writefln("%s%s %s", '\t'.repeat(depth), name.quote, node.value.quote);
 			else
-			if (name == "gun" || name == "engine" || name == "turret")
+			if (name == "gun" || name == "engine" || name == "turret" || name == "explode")
 			{
 				node.iterLeaves(
 					(string[] path)
