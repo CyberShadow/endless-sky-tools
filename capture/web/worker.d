cc6e49bd6ce8f1daf81885a17de760b6778cf087
Vladimir Panteleev <git@thecybershadow.net>
2018-05-10 20:09:01 +0000

    capture: [WIP] allocator debugging

--- capture/web/worker.d
+++ capture/web/worker.d
@@ -1,10 +1,14 @@
 module worker;
 
+import std.experimental.allocator;
+import std.stdio;
+
 // Build with https://github.com/CyberShadow/dscripten-tools
 
 import core.stdc.stdio;
 import core.bitop;
 import core.checkedint;
+import core.stdc.stdlib;
 
 import ldc.arrayinit;
 import dscripten.standard;
@@ -18,15 +22,56 @@ import calcnp;
 import common;
 
 extern(C)
-void main() @nogc
+	void main() //@nogc
 {
 	//gc_init();
+	/*
+	while (true)
+	{
+		printf("----------------------------\n");
+		Allocator0s allocator0s;
+		.allocator0s = &allocator0s;
 
-	Allocator allocator;
-	.allocator = &allocator;
+		Allocator allocator;
+		.allocator = &allocator;
 
-	auto problem = getProblem();
-	auto result = calculate(problem);
+		auto p = make!int(allocator);
+		printf("%p\n", p);
 
-	printf("Win odds: %f%% (1 in %d)\n", 100 * result.winOdds, cast(int)(1 / result.winOdds));
+		allocator0s.reportStatistics(std.stdio.stdout);
+
+		auto problem = getProblem();
+		p = make!int(allocator); printf("%p\n", p);
+		auto result = calculate(problem);
+
+		printf("Win odds: %f%% (1 in %d)\n", 100 * result.winOdds, cast(int)(1 / result.winOdds));
+
+		p = make!int(allocator); printf("%p\n", p);
+
+		//printf("%d\n", allocator.deallocateAll());
+
+	}
+	*/
+
+	/*
+	while (true)
+	{
+		printf("----------------------------\n");
+		static __gshared Allocator0s allocator0s;
+		allocator0s = Allocator0s.init;
+
+		.allocator0s = &allocator0s;
+
+		Allocator allocator;
+		.allocator = &allocator;
+
+		auto p = make!int(allocator);
+		printf("%p\n", p);
+
+        printf("StatsCollector[%p].reportStatistics\n", &allocator0s);
+		allocator0s.reportStatistics(std.stdio.stdout);
+		printf("%d\n", allocator.deallocateAll());
+	}
+	*/
+	allocTest();
 }
